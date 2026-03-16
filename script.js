9a382a86c112330f32d22529a3334a097630fdef
tranthanhmen9-cpu <tranthanhmen9@gmail.com>
2026-03-16 22:00:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(){
+window.onload = function(){
 
 const passwordBtn = document.getElementById("passwordBtn")
 const openLetterBtn = document.getElementById("openLetterBtn")
@@ -8,16 +8,15 @@ const noBtn = document.getElementById("noBtn")
 passwordBtn.onclick = function(){
 
 let password = document.getElementById("passwordInput").value
-let correctPassword = "2501200510032005"
 
-if(password === correctPassword){
+if(password === "123456"){
 
 document.getElementById("passwordBox").style.display="none"
 document.getElementById("letterBox").classList.remove("hidden")
 
 }else{
 
-document.getElementById("errorText").innerText="Sai mật khẩu rồi 😢"
+document.getElementById("errorText").innerText="Sai mật khẩu"
 
 }
 
@@ -32,10 +31,10 @@ document.getElementById("confessionBox").classList.remove("hidden")
 }
 
 
-noBtn.onmouseover = function(){
+noBtn.onmouseover=function(){
 
-let x=Math.random()*window.innerWidth*0.7
-let y=Math.random()*window.innerHeight*0.5
+let x=Math.random()*400
+let y=Math.random()*200
 
 noBtn.style.position="absolute"
 noBtn.style.left=x+"px"
@@ -49,68 +48,6 @@ yesBtn.onclick=function(){
 document.getElementById("confessionBox").style.display="none"
 document.getElementById("successBox").classList.remove("hidden")
 
-startFireworks()
-
-}
-
-
-
-function startFireworks(){
-
-const canvas=document.getElementById("fireworks")
-const ctx=canvas.getContext("2d")
-
-canvas.width=window.innerWidth
-canvas.height=window.innerHeight
-
-let particles=[]
-
-function createFirework(){
-
-let x=Math.random()*canvas.width
-let y=Math.random()*canvas.height
-
-for(let i=0;i<80;i++){
-
-particles.push({
-
-x:x,
-y:y,
-vx:(Math.random()-0.5)*8,
-vy:(Math.random()-0.5)*8
-
-})
-
 }
 
 }
-
-function animate(){
-
-ctx.clearRect(0,0,canvas.width,canvas.height)
-
-if(Math.random()<0.05){
-createFirework()
-}
-
-particles.forEach(p=>{
-
-ctx.beginPath()
-ctx.arc(p.x,p.y,3,0,Math.PI*2)
-ctx.fillStyle="gold"
-ctx.fill()
-
-p.x+=p.vx
-p.y+=p.vy
-
-})
-
-requestAnimationFrame(animate)
-
-}
-
-animate()
-
-}
-
-})
